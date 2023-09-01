6debfc8c2607e805d0ac6d8169322a824a1e0092
R4venSlayer <126794953+R4venSlayer@users.noreply.github.com>
2023-09-01 11:04:49 -0500

    Update extraer.py

--- extraer.py
+++ extraer.py
@@ -3,20 +3,24 @@ import datetime
 import boto3
 from io import BytesIO
 
+
 def get_response():
     # URL de la página a hacer web scraping
     return urlopen('https://www.eltiempo.com')
+
 
 def upload_file(archivo_m, bucket, archivo):
     s3 = boto3.client('s3')
     # Subir el archivo en memoria a S3
     s3.upload_fileobj(archivo_m, bucket, 'news/raw/' + archivo)
 
+
 def name_file():
     # Generamos el nombre del archivo usando la fecha actual
     fecha_actual = datetime.datetime.now().strftime('%Y-%m-%d')
     nombre_archivo = f'{fecha_actual}.html'
     return nombre_archivo
+
 
 def main():
     # Realizamos la solicitud GET a la URL
@@ -37,6 +41,7 @@ def main():
         print('Web scraping completado. El contenido se ha subido al bucket')
     else:
         print('Error en la página web')
+
 
 if __name__ == "__main__":
     main()
